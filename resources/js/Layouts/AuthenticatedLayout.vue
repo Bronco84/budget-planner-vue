1018dd904b205c8beeee54d6d49baacbc21ff78d
Brad McColey <bamccoley@gmail.com>
2025-11-01 16:23:48 -0500

    refactor: Remove Dashboard navigation link
    Remove the Dashboard link from the main navigation bar since users can always click the logo to return to the dashboard, and it reduces navigation clutter.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -40,19 +40,13 @@ initializeTheme();
                             <div
                                 class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
                             >
-                                <NavLink
-                                    :href="route('dashboard')"
-                                    :active="route().current('dashboard')"
-                                >
-                                    Dashboard
-                                </NavLink>
                             </div>
                         </div>
 
                         <div class="hidden sm:ms-6 sm:flex sm:items-center space-x-4">
                             <!-- Theme Toggle -->
                             <ThemeToggle />
-                            
+
                             <!-- Settings Dropdown -->
                             <div class="relative ms-3">
                                 <Dropdown align="right" width="48">
